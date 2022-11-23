0dc4b78fbef668b033bd4ad9ede4de96a5babd08
aravindmetku <email>
2022-11-23 12:47:13 +0530

    CID-1136 fix

--- src/github-util.js
+++ src/github-util.js
@@ -1,15 +1,18 @@
 const github = require("@actions/github");
 
 function getGitHubRepoName() {
-    return github.context.repo.repo
+    // return github.context.repo.repo
+    return 'repo'
 }
 
 function getGitHubRepoDescription() {
-    return github.context.repo.repo
+    // return github.context.repo.repo
+    return 'repo'
 }
 
 function getGitHubOrgName() {
-    return github.context.repo.owner
+    // return github.context.repo.owner
+    return 'leanix'
 }
 
 module.exports = {getGitHubOrgName, getGitHubRepoDescription, getGitHubRepoName}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 const core = require('@actions/core');
 const fs = require("fs");
+
 const {authenticate} = require("./mtm")
+const {registerService} = require("./discovery-api-client")
 const {getGitHubOrgName, getGitHubRepoDescription, getGitHubRepoName} = require("./github-util")
+
+
+
+const host = core.getInput('host');
+const token = core.getInput('api-token');
+const sbomFilePath = core.getInput('sbom-path');
+const data = core.getInput('additional-data');
+const serviceName = core.getInput('service-name');
+const sourceType = core.getInput('source-type');
+
+main({
+    host,
+    token,
+    sbomFilePath,
+    data,
+    serviceName,
+    sourceType
+}).then().catch(e => core.setFailed(`Failed to register service. Error: ${e.message}`))
 
 function getSbomFile(sbomFilePath) {
     console.log("Getting generated sbom file");
     if (!sbomFilePath || !fs.existsSync(sbomFilePath)) {
-        core.warning("Could not find dependency file");
+        core.warning("Could not find SBOM file. Follow the documentation in README.md to learn how to generate SBOM file.");
         return null;
     }
 
@@ -24,62 +44,20 @@ function getSbomFile(sbomFilePath) {
 function validateInputs(inputs) {
     const {token} = inputs
 
-    if(!token) {
+    if (!token) {
         throw new Error('Please add LXVSM_TECHNICAL_USER_TOKEN in your secrets. Generate the token from the VSM workspace under technical users tab.')
     }
 }
 
-function registerService(axios, {id, sbomFile, sourceType, sourceInstance, name, description,}) {
-    console.log(`Registering service and SBOM with following details. id: ${id}, sourceType: ${sourceType}, sourceInstance: ${sourceInstance}, name: ${name}, description: ${description}`)
-
-    return Promise.resolve()
-    // const options = createConnectorCallInit(host, bearerToken);
-    // const formData = new FormData();
-    // formData.append("id", id);
-    // formData.append("sourceType", sourceType);
-    // formData.append("sourceInstance", sourceInstance);
-    // formData.append("name", name);
-    // formData.append("description", description);
-    // // formData.append("data", JSON.stringify(data));
-    // formData.append("bom", sbomFile);
-    //
-    // return new Promise((resolve, reject) => {
-    //     const req = formData.submit(options, (err, res) => {
-    //         if (err) {
-    //             console.error(`Http-response to discovery API was not ok. http status response: ${res.statusCode}. Error message: ${err.message}`);
-    //             return reject(new Error(err.message));
-    //         }
-    //         if (res.statusCode < 200 || res.statusCode > 299) {
-    //             return reject(new Error(`HTTP status code ${res.statusCode}`));
-    //         }
-    //         const body = [];
-    //         res.on("data", (chunk) => body.push(chunk));
-    //         res.on("end", () => {
-    //             const resString = Buffer.concat(body).toString();
-    //             resolve(resString);
-    //         });
-    //     });
-    // })
-    //     .then((result) => {
-    //         return result;
-    //     })
-    //     .catch((reason) => {
-    //         console.error("Couldn't access the API");
-    //         throw new Error(reason);
-    //     });
-}
-
-
 async function main(inputs) {
     validateInputs(inputs)
 
-    const {token, host, sbomFilePath,} = inputs
+    const {token, host, sbomFilePath, sourceType} = inputs
     const axios = await authenticate(host, token)
 
     // todo get from inputs over defauls
     const serviceName = getGitHubRepoName()
     const sourceInstance = getGitHubOrgName()
-    const sourceType = 'cicd'
     const defaults = {
         id: `${sourceType}-${sourceInstance}-${serviceName}`,
         sourceType,
@@ -97,9 +75,3 @@ async function main(inputs) {
         sbomFile: getSbomFile(sbomFilePath)
     })
 }
-
-const host = core.getInput('host');
-const token = core.getInput('api-token');
-const sbomFilePath = core.getInput('sbom-path');
-// const serviceName = core.getInput('sbom-path');
-main({host, token, sbomFilePath}).then().catch(e => core.setFailed(`Failed to register service. Error: ${e.message}`))
