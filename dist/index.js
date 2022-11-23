8511022176244b5a4ea98326439c743d7d3e2212
leanix-ci <leanix-ci@users.noreply.github.com>
2022-11-23 06:01:04 +0000

    Update dist files

--- dist/index.js
+++ dist/index.js
@@ -1,92 +1,6 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 301:
-/***/ ((module) => {
-
-// const formData = new FormData();
-
-
-function registerService(axios, {id, sbomFile, sourceType, sourceInstance, name, description,}) {
-    console.log(`Registering service and SBOM with following details. id: ${id}, sourceType: ${sourceType}, sourceInstance: ${sourceInstance}, name: ${name}, description: ${description}`)
-
-    return Promise.resolve()
-    // const options = createConnectorCallInit(host, bearerToken);
-    // const formData = new FormData();
-    // formData.append("id", id);
-    // formData.append("sourceType", sourceType);
-    // formData.append("sourceInstance", sourceInstance);
-    // formData.append("name", name);
-    // formData.append("description", description);
-    // // formData.append("data", JSON.stringify(data));
-    // formData.append("bom", sbomFile);
-    //
-    // return new Promise((resolve, reject) => {
-    //     const req = formData.submit(options, (err, res) => {
-    //         if (err) {
-    //             console.error(`Http-response to discovery API was not ok. http status response: ${res.statusCode}. Error message: ${err.message}`);
-    //             return reject(new Error(err.message));
-    //         }
-    //         if (res.statusCode < 200 || res.statusCode > 299) {
-    //             return reject(new Error(`HTTP status code ${res.statusCode}`));
-    //         }
-    //         const body = [];
-    //         res.on("data", (chunk) => body.push(chunk));
-    //         res.on("end", () => {
-    //             const resString = Buffer.concat(body).toString();
-    //             resolve(resString);
-    //         });
-    //     });
-    // })
-    //     .then((result) => {
-    //         return result;
-    //     })
-    //     .catch((reason) => {
-    //         console.error("Couldn't access the API");
-    //         throw new Error(reason);
-    //     });
-}
-
-
-module.exports = {registerService}
-
-/***/ }),
-
-/***/ 225:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const axios = (__nccwpck_require__(141)["default"]);
-
-async function authenticate(host, token) {
-    const encodedToken = Buffer.from(`apitoken:${token}`).toString('base64');
-    const data = new URLSearchParams({ grant_type: 'client_credentials' }).toString();
-    try {
-        const res = await axios.post(`https://${host}/services/mtm/v1/oauth2/token`, data, {
-            headers: {
-                Authorization: `Basic ${encodedToken}`,
-                'content-type': 'application/x-www-form-urlencoded'
-            }
-        });
-
-        console.info(`Successfully generated JWT token.`);
-
-        return axios.create({
-            baseURL: `https://eu-vsm.leanix.net/services/vsm/discovery/v1`,
-            headers: {
-                Authorization: `Bearer ${res.data.access_token}`
-            }
-        });
-    } catch (e) {
-        console.error('Failed to authenticate using provided technical user token.. terminating');
-        throw new Error('Failed to authenticate using system token.. terminating');
-    }
-}
-
-
-module.exports = {authenticate}
-
-/***/ }),
-
 /***/ 836:
 /***/ ((module) => {
 
@@ -162,9 +76,8 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(836);
 const github = __nccwpck_require__(800);
-const {authenticate} = __nccwpck_require__(225)
-const {registerService} = __nccwpck_require__(301)
 const fs = __nccwpck_require__(147);
+const axios = (__nccwpck_require__(141)["default"]);
 
 function getSbomFile(sbomFilePath) {
     console.log("Getting generated sbom file");
@@ -200,11 +113,77 @@ function validateInputs(inputs) {
     // todo complete
 }
 
+async function authenticate(host, token) {
+    const encodedToken = Buffer.from(`apitoken:${token}`).toString('base64');
+    const data = new URLSearchParams({ grant_type: 'client_credentials' }).toString();
+    try {
+        const res = await axios.post(`https://${host}/services/mtm/v1/oauth2/token`, data, {
+            headers: {
+                Authorization: `Basic ${encodedToken}`,
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        console.info(`Successfully generated JWT token.`);
+
+        return axios.create({
+            baseURL: `https://eu-vsm.leanix.net/services/vsm/discovery/v1`,
+            headers: {
+                Authorization: `Bearer ${res.data.access_token}`
+            }
+        });
+    } catch (e) {
+        console.error('Failed to authenticate using provided technical user token.. terminating');
+        throw new Error('Failed to authenticate using system token.. terminating');
+    }
+}
+
+function registerService(axios, {id, sbomFile, sourceType, sourceInstance, name, description,}) {
+    console.log(`Registering service and SBOM with following details. id: ${id}, sourceType: ${sourceType}, sourceInstance: ${sourceInstance}, name: ${name}, description: ${description}`)
+
+    return Promise.resolve()
+    // const options = createConnectorCallInit(host, bearerToken);
+    // const formData = new FormData();
+    // formData.append("id", id);
+    // formData.append("sourceType", sourceType);
+    // formData.append("sourceInstance", sourceInstance);
+    // formData.append("name", name);
+    // formData.append("description", description);
+    // // formData.append("data", JSON.stringify(data));
+    // formData.append("bom", sbomFile);
+    //
+    // return new Promise((resolve, reject) => {
+    //     const req = formData.submit(options, (err, res) => {
+    //         if (err) {
+    //             console.error(`Http-response to discovery API was not ok. http status response: ${res.statusCode}. Error message: ${err.message}`);
+    //             return reject(new Error(err.message));
+    //         }
+    //         if (res.statusCode < 200 || res.statusCode > 299) {
+    //             return reject(new Error(`HTTP status code ${res.statusCode}`));
+    //         }
+    //         const body = [];
+    //         res.on("data", (chunk) => body.push(chunk));
+    //         res.on("end", () => {
+    //             const resString = Buffer.concat(body).toString();
+    //             resolve(resString);
+    //         });
+    //     });
+    // })
+    //     .then((result) => {
+    //         return result;
+    //     })
+    //     .catch((reason) => {
+    //         console.error("Couldn't access the API");
+    //         throw new Error(reason);
+    //     });
+}
+
 
 async function main(inputs) {
     const {token, host, sbomFilePath, } = inputs
-    const bearerToken = await authenticate(token, host)
+    const axios = await authenticate(token, host)
 
+    // todo get from inputs over defauls
     const serviceName = getGitHubRepoName()
     const sourceInstance = getGitHubOrgName()
     const sourceType = 'cicd'
@@ -216,13 +195,13 @@ async function main(inputs) {
         ...inputs
     }
 
-    await registerService(host, bearerToken, {
+    await registerService(axios, {
         ...withOverrideDefaults,
         sbomFile: getSbomFile(sbomFilePath)
     })
 }
 
-main({}).then()
+main({host: core.getInput('host'), token: core.getInput('api-token')}).then()
 })();
 
 module.exports = __webpack_exports__;
