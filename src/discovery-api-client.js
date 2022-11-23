f1f0fc620042a2a0754e596fbeca4c97b9edf0d0
aravindmetku <email>
2022-11-23 16:29:26 +0530

    CID-1136 formatter and new github action

--- src/discovery-api-client.js
+++ src/discovery-api-client.js
@@ -1,20 +1,26 @@
 const FormData = require("form-data");
 
-function registerService(axios, {id, sourceType, sourceInstance, name, description, data}, sbomFile) {
-    console.log(`Registering service and SBOM with following details. id: ${id}, sourceType: ${sourceType}, sourceInstance: ${sourceInstance}, name: ${name}, description: ${description}`)
+function registerService(
+  axios,
+  { id, sourceType, sourceInstance, name, description, data },
+  sbomFile
+) {
+  console.log(
+    `Registering service and SBOM with following details. id: ${id}, sourceType: ${sourceType}, sourceInstance: ${sourceInstance}, name: ${name}, description: ${description}`
+  );
 
-    const formData = new FormData();
-    formData.append("id", id);
-    formData.append("sourceType", sourceType);
-    formData.append("sourceInstance", sourceInstance);
-    formData.append("name", name);
-    formData.append("description", description);
-    formData.append("data", data);
-    if(sbomFile !== null) {
-        formData.append("bom", sbomFile);
-    }
+  const formData = new FormData();
+  formData.append("id", id);
+  formData.append("sourceType", sourceType);
+  formData.append("sourceInstance", sourceInstance);
+  formData.append("name", name);
+  formData.append("description", description);
+  formData.append("data", data);
+  if (sbomFile !== null) {
+    formData.append("bom", sbomFile);
+  }
 
-    return axios.post('/service', formData)
+  return axios.post("/service", formData);
 }
 
-module.exports = {registerService}
+module.exports = { registerService };
