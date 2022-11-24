850ecf8b381ab9d987efba7e8c723c5f0b3abd7b
aravindmetku <email>
2022-11-24 17:02:57 +0530

    CID-1136 documentation - description

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ async function main(dryRun, inputs) {
 
   const sbomFile = getSbomFile(sbomFilePath);
   const serviceName = name || getGitHubRepoName();
-  const serviceDescription = description || getGitHubRepoName();
+  const serviceDescription = description || `This service has been brought in by the GitHub action (${getGitHubRepoName()})`;
   const _sourceInstance = sourceInstance || getGitHubOrgName();
   const _data = data && typeof data === "string" ? data : "{}";
 
