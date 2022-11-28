6814505715a6d8e4a487eb8c69c47a33673e8016
Vince <vincent.groves@leanix.net>
2022-11-28 18:07:28 +0100

    added logic to fetch repo id

--- src/github-util.js
+++ src/github-util.js
@@ -1,4 +1,6 @@
 const github = require("@actions/github");
+const core = require('@actions/core');
+
 
 function getGitHubRepoName() {
   return github.context.repo.repo;
@@ -8,4 +10,16 @@ function getGitHubOrgName() {
   return github.context.repo.owner;
 }
 
-module.exports = { getGitHubOrgName, getGitHubRepoName };
+function getRepoId() {
+  const token = core.getInput("github-token")
+  const context= github.context
+  const octo = github.getOctokit(token)
+  const { data.node_id: repoId } = await octo.rest.repos.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  })
+
+  return repoId;
+}
+
+module.exports = { getGitHubOrgName, getGitHubRepoName, getRepoId};
