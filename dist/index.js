f92d026b6d4117c1df3d124bc7777d8eddd08110
leanix-ci <leanix-ci@users.noreply.github.com>
2022-11-29 06:16:19 +0000

    Update dist files

--- dist/index.js
+++ dist/index.js
@@ -11833,7 +11833,7 @@ async function getRepoId(token) {
   const context = github.context;
   const octokit = github.getOctokit(token);
   const {
-    data: { id: repoId },
+    data: { node_id: repoId },
   } = await octokit.rest.repos.get({
     owner: context.repo.owner,
     repo: context.repo.repo,
