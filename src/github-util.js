4f48a1419505608ab9013f9b609484a58a8e95c1
alfredo-mfaria <alfredo.faria88@gmail.com>
2023-05-26 17:57:38 +0200

    CID-1587 RepoId from github workflow context

--- src/github-util.js
+++ src/github-util.js
@@ -1,5 +1,4 @@
 const github = require("@actions/github");
-const core = require("@actions/core");
 
 function getGitHubRepoName() {
   return github.context.repo.repo;
@@ -9,25 +8,4 @@ function getGitHubOrgName() {
   return github.context.repo.owner;
 }
 
-function getGitHubRepoId() {
-  // core.info("##############################")
-  // const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-  // core.info("token: " + process.env.GITHUB_TOKEN);
-  //
-  // const query = `
-  //   {
-  //     repository(owner:"${github.context.repo.owner}", name:"${github.context.repo.repo}") {
-  //       id
-  //     }
-  //   }
-  //   `;
-  // core.info("Query: " + query);
-  //
-  // const response = await octokit.graphql();
-  //
-  // core.info("Response: " + response);
-
-  return "response.repository.id";
-}
-
-module.exports = { getGitHubOrgName, getGitHubRepoName, getGitHubRepoId };
+module.exports = { getGitHubOrgName, getGitHubRepoName };
